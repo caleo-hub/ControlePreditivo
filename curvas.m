0430eefc02006cfcb443816a8bbaa08638d576ce
unknown <caleomenesessantos@gmail.com>
2022-07-02 18:50:22 -0300

    charts

---
+++ curvas.m
@@ -0,0 +1,143 @@
+close all
+dados_MPC_sem_restricao()
+Simulation_Time = 100;
+out = sim('modelo_mpc_sem_restricao',Simulation_Time);
+
+figure('Name','Linear - Sem Restrição')
+subplot(3,1,1)
+
+plot(y_linear_sr.Time,y_linear_sr.Data,'LineWidth',1)
+hold on
+plot(referencia_sr.Time,referencia_sr.Data,'--')
+grid on
+title('Y[(t)]')
+xlabel('tempo(s)')
+ylabel('Nível(cm)')
+legend('h_1','h_2','r_h_1','r_h_2','location','east')
+
+
+subplot(3,1,2)
+plot(du_linear_sr.Time,du_linear_sr.Data,'LineWidth',1)
+grid on
+title('dU[(t)]')
+xlabel('tempo(s)')
+ylabel('Tensão(V)')
+legend('Bomba_1','Bomba_2','location','east')
+
+subplot(3,1,3)
+plot(u_linear_sr.Time,u_linear_sr.Data,'LineWidth',1)
+grid on
+title('U[(t)]')
+xlabel('tempo(s)')
+ylabel('Tensão(V)')
+legend('Bomba_1','Bomba_2','location','east')
+sgtitle('Linear - Sem Restrição')
+
+% -----------------------------------
+figure('Name','Não Linear - Sem Restrição')
+subplot(3,1,1)
+
+plot(y_nao_linear_sr.Time,y_nao_linear_sr.Data,'LineWidth',1)
+hold on
+plot(referencia_sr.Time,referencia_sr.Data,'--')
+grid on
+title('Y[(t)]')
+xlabel('tempo(s)')
+ylabel('Nível(cm)')
+legend('h_1','h_2','r_h_1','r_h_2','location','east')
+
+
+subplot(3,1,2)
+plot(du_nao_linear_sr.Time,du_nao_linear_sr.Data,'LineWidth',1)
+grid on
+title('dU[(t)]')
+xlabel('tempo(s)')
+ylabel('Tensão(V)')
+legend('Bomba_1','Bomba_2','location','east')
+
+subplot(3,1,3)
+plot(u_nao_linear_sr.Time,u_nao_linear_sr.Data,'LineWidth',1)
+grid on
+title('U[(t)]')
+xlabel('tempo(s)')
+ylabel('Tensão(V)')
+legend('Bomba_1','Bomba_2','location','east')
+sgtitle('Não Linear - Sem Restrição')
+
+%% --------------------------------------------------------------
+dados_MPC_com_restricao()
+Simulation_Time = 50;
+out = sim('modelo_mpc_com_restricao',Simulation_Time);
+
+figure('Name','Linear - Com Restrição')
+subplot(3,1,1)
+
+plot(y_linear_cr.Time,y_linear_cr.Data,'LineWidth',1)
+hold on
+plot(referencia_cr.Time,referencia_cr.Data,'--')
+grid on
+title('Y[(t)]')
+xlabel('tempo(s)')
+ylabel('Nível(cm)')
+legend('h_1','h_2','r_h_1','r_h_2','location','east')
+
+
+subplot(3,1,2)
+plot(du_linear_cr.Time,du_linear_cr.Data,'LineWidth',1)
+grid on
+title('dU[(t)]')
+xlabel('tempo(s)')
+ylabel('Tensão(V)')
+legend('Bomba_1','Bomba_2','location','east')
+
+subplot(3,1,3)
+plot(u_linear_cr.Time,u_linear_cr.Data,'LineWidth',1)
+grid on
+title('U[(t)]')
+xlabel('tempo(s)')
+ylabel('Tensão(V)')
+legend('Bomba_1','Bomba_2','location','east')
+sgtitle('Linear - Com Restrição')
+
+% -----------------------------------
+figure('Name','Não Linear - Com Restrição')
+subplot(3,1,1)
+
+plot(y_nao_linear_cr.Time,y_nao_linear_cr.Data,'LineWidth',1)
+hold on
+plot(referencia_cr.Time,referencia_cr.Data,'--')
+grid on
+title('Y[(t)]')
+xlabel('tempo(s)')
+ylabel('Nível(cm)')
+legend('h_1','h_2','r_h_1','r_h_2','location','east')
+
+
+subplot(3,1,2)
+plot(du_nao_linear_cr.Time,du_nao_linear_cr.Data,'LineWidth',1)
+grid on
+title('dU[(t)]')
+xlabel('tempo(s)')
+ylabel('Tensão(V)')
+legend('Bomba_1','Bomba_2','location','east')
+
+subplot(3,1,3)
+plot(u_nao_linear_cr.Time,u_nao_linear_cr.Data,'LineWidth',1)
+grid on
+title('U[(t)]')
+xlabel('tempo(s)')
+ylabel('Tensão(V)')
+legend('Bomba_1','Bomba_2','location','east')
+sgtitle('Não Linear - Com Restrição')
+
+
+%% Saving Figures
+FigList = findobj(allchild(0), 'flat', 'Type', 'figure');
+FolderName = tempdir;
+for iFig = 1:length(FigList)
+  FigHandle = FigList(iFig);
+  FigName   = get(FigHandle, 'Name');
+%   savefig(FigHandle, fullfile(FolderName, FigName, '.fig'));
+  saveas(FigHandle,FigName,'png')
+
+end
---
+++ dados_MPC_com_restricao.m
@@ -0,0 +1,150 @@
+clear,clc
+dadosTanqueQuatro();
+
+N = 20;
+Nu = 5;
+ts= 0.1;
+rho = 0.001;
+
+sysd = c2d(ss(A,B,C,D),ts,'zoh');
+Ad = sysd.A;
+Bd = sysd.B;
+Cd = sysd.C;
+Dd = sysd.D;
+
+
+%% Sistema expandido
+[m,n] = size(Ad);
+[n_saidas,q] = size(Cd);
+[rr,n_entradas] = size(Bd);
+
+Aa = [Ad, zeros(m,n_saidas); Cd*Ad, eye(n_saidas)];
+Ba = [Bd;Cd*Bd];
+Ca = [zeros(n_saidas,m) eye(n_saidas)];
+Da = Dd;
+
+Qy =  eye(n_saidas);
+Ru = rho*eye(n_entradas);
+
+Ap = [];
+Bp = [];
+Qp = [];
+Rp = [];
+Cp = [];
+Wr = [];
+
+%% Matrizes baseadas em N
+% Construção matrizes caligráficas A, Q, C e W
+for linha=1:N
+    Ap = [Ap; Aa^linha];
+    Qp =blkdiag(Qp,Qy);
+    Cp =blkdiag(Cp,Ca);
+    Wr = [Wr; eye(n_entradas)];
+end
+
+
+%% Matrizes baseadas em Nu
+% Construção das matrizes Caligráficas R
+for i=1:Nu
+    Rp = blkdiag(Rp,Ru);
+end
+
+% Construção das matrizes Caligráficas B
+for linha=1:N
+    aux = [];
+    for coluna=1:Nu
+        
+        
+        
+        if coluna<=linha
+            aux = [aux Aa^(linha-coluna)*Ba];
+        else
+            aux = [aux zeros(size(Aa*Ba))];
+        end
+        
+    end
+    Bp = [Bp;aux];
+end    
+
+%% Ganho fixo do Model Predictive Control (mpc)
+Kmpc = inv(Bp'*Cp'*Qp*Cp*Bp + Rp)*Bp'*Cp'*Qp';
+Kmpc = Kmpc(1:n_entradas,:); % Obtendo somente os n_entradas primeiros ganhos para entrada do sistema
+Rl = Cp*Ap;
+
+
+%% Restrições Definição
+M = [];
+for linha = 1:Nu
+    aux = [];
+    for coluna = 1:Nu
+        if coluna<=linha
+            aux = [aux eye(n_entradas)];
+        else
+            aux = [aux zeros(n_entradas)];
+        end
+    end 
+    M = [M;aux];    
+end
+
+%Colocar valores sempre positivo
+ymax = 2.5; 
+ymin = 1;
+
+
+% Tensão adrão - 3V
+%Tensão Máxima 9+3 = 12V
+%Tensão Mínima 3-3 = 0V
+
+u1max = 9;%+9V
+u1min = 3;%-3V
+u2max = 9;%+9V
+u2min = 3; %-3V
+
+du1max = 10;
+du2max = 10;
+du1min = 10;
+du2min = 10;
+
+ya=[1 0;-1 0;0 1;0 -1];yb=[ymax; ymin;ymax; ymin];
+ua=[1 0;-1 0;0 1;0 -1];ub=[u1max;u1min;u2max;u2min];
+dua=[1 0;-1 0;0 1;0 -1];dub=[du1max;du1min;du2max;du2min];
+
+Ya = [];
+Yb = [];
+for linha=1:N
+    Ya = blkdiag(Ya,ya);
+    Yb = [Yb;yb];
+    
+    
+end
+
+
+Dua = [];
+Ua = [];
+Ub = [];
+Dub = [];
+for i=1:Nu
+    Dua = blkdiag(Dua,dua);
+    Ua = blkdiag(Ua,ua);
+    Ub = [Ub;ub];
+    Dub = [Dub;dub];
+    
+end
+
+var.N = N;
+var.Nu = Nu;
+var.Bp = Bp;
+var.Cp=Cp;
+var.Qp = Qp;
+var.Rp = Rp;
+var.Ap = Ap;
+var.Ya = Ya;
+var.Yb = Yb;
+var.Ua = Ua;
+var.Ub = Ub;
+var.M = M;
+var.Dua = Dua;
+var.Dub = Dub;
+
+var_bus_info = Simulink.Bus.createObject(var);
+var_bus = evalin('base', var_bus_info.busName);
